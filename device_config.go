7bb1928bc2eecb7eda76f4e17c806f3397ffc6fa
Eric Daniels <eric@erdaniels.com>
2022-08-13 15:17:03 -1000

    fixes

--- device_config.go
+++ device_config.go
@@ -50,28 +50,37 @@ func (d *DeviceConfig) cptrClone() (*C.ma_device_config, error) {
 	deviceConfig.dataCallback = d.DataCallback
 	deviceConfig.stopCallback = d.StopCallback
 	deviceConfig.pUserData = unsafe.Pointer(d.PUserData)
+
 	deviceConfig.resampling.algorithm = C.ma_resample_algorithm(d.Resampling.Algorithm)
 	deviceConfig.resampling.linear.lpfOrder = C.uint(d.Resampling.Linear.LpfOrder)
 	deviceConfig.resampling.speex.quality = C.int(d.Resampling.Speex.Quality)
-	convertSubConfig := func(to *C.struct___95, from SubConfig) {
-		to.pDeviceID = (*C.ma_device_id)(from.DeviceID)
-		to.format = C.ma_format(from.Format)
-		to.channels = C.uint(from.Channels)
-		for i := 0; i < len(to.channelMap); i++ {
-			to.channelMap[i] = (C.uchar)(from.ChannelMap[i])
-		}
-		to.shareMode = C.ma_share_mode(from.ShareMode)
+
+	deviceConfig.playback.pDeviceID = (*C.ma_device_id)(d.Playback.DeviceID)
+	deviceConfig.playback.format = C.ma_format(d.Playback.Format)
+	deviceConfig.playback.channels = C.uint(d.Playback.Channels)
+	for i := 0; i < len(deviceConfig.playback.channelMap); i++ {
+		deviceConfig.playback.channelMap[i] = (C.uchar)(d.Playback.ChannelMap[i])
 	}
-	convertSubConfig(&deviceConfig.playback, d.Playback)
-	convertSubConfig(&deviceConfig.capture, d.Capture)
+	deviceConfig.playback.shareMode = C.ma_share_mode(d.Playback.ShareMode)
+
+	deviceConfig.capture.pDeviceID = (*C.ma_device_id)(d.Capture.DeviceID)
+	deviceConfig.capture.format = C.ma_format(d.Capture.Format)
+	deviceConfig.capture.channels = C.uint(d.Capture.Channels)
+	for i := 0; i < len(deviceConfig.capture.channelMap); i++ {
+		deviceConfig.capture.channelMap[i] = (C.uchar)(d.Capture.ChannelMap[i])
+	}
+	deviceConfig.capture.shareMode = C.ma_share_mode(d.Capture.ShareMode)
+
 	deviceConfig.wasapi.noAutoConvertSRC = C.uchar(d.Wasapi.NoHardwareOffloading)
 	deviceConfig.wasapi.noDefaultQualitySRC = C.uchar(d.Wasapi.NoDefaultQualitySRC)
 	deviceConfig.wasapi.noAutoStreamRouting = C.uchar(d.Wasapi.NoAutoStreamRouting)
 	deviceConfig.wasapi.noHardwareOffloading = C.uchar(d.Wasapi.NoHardwareOffloading)
+
 	deviceConfig.alsa.noMMap = C.uint(d.Alsa.NoMMap)
 	deviceConfig.alsa.noAutoFormat = C.uint(d.Alsa.NoAutoFormat)
 	deviceConfig.alsa.noAutoChannels = C.uint(d.Alsa.NoAutoChannles)
 	deviceConfig.alsa.noAutoResample = C.uint(d.Alsa.NoAutoResample)
+
 	deviceConfig.pulse.pStreamNameCapture = (*C.char)(d.Pulse.StreamNameCapture)
 	deviceConfig.pulse.pStreamNamePlayback = (*C.char)(d.Pulse.StreamNamePlayback)
 
